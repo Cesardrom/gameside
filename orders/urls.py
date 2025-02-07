7a6f96ffc8dcbbc187efa7326e2c16bc36148249
David <dricciodeabreu1@gmail.com>
2025-02-07 16:32:24 +0000

    Doc update

--- orders/urls.py
+++ orders/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('add/', views.add_order, name='add-order'),
     path('<int:order_pk>/games/', views.order_game_list, name='order-game-list'),
     path(
-        '<int:order_pk>/games/add/<str:game_slug>/',
+        '<int:order_pk>/games/add/',
         views.add_game_to_order,
         name='add-game-to-order',
     ),
